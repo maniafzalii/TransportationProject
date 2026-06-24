0ac31e3d1381af00b44c90827a06c4f01f002a94
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-06-24 21:28:11 +0330

    new validations edit - use them

--- shayan/Validation.py
+++ shayan/Validation.py
@@ -1,20 +1,60 @@
 import re
 
+RESET = '\033[0m'
+RED = '\033[91m'
 
 def validate_password(password):
     pattern = r"^(?=.*[@&])(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9@&]*$"
     if re.match(pattern, password):
-        print("Valid Password")
+        return True
+
     else:
         print("Not Valid password , try again !!")
-
+        return False
 
 def validate_email(email):
     pattern = r".+@[a-zA-Z]+\.[a-zA-Z]{2,}$"
     if re.match(pattern, email):
-        print("email saved")
+        return True
     else:
         print("not valid email ! try again")
+        return False
 
+
+def letter_please(prompt):
+    while True:
+        value = input(prompt).strip()
+        if value.isalpha() == True:
+            return value
+        else:
+            print(f"{RED}use a-z A-Z as a correct input !{RESET}\n")
+
+
+def number_please(prompt):
+    while True:
+        value = input(prompt)
+        if value.isdigit() == True:
+            return int(value)
+        else:
+            print(f"{RED}use numbers only !!! {RESET}\n")
+
+
+def add_line():
+    stations = []
+    print("\n--- Adding new line ---\n")
+    name = letter_please("Enter line name : ")
+    origin = letter_please("Origin city : ")
+    destination = letter_please("Destination city : ")
+    count = number_please("Enter the number of Stations : ")
+
+    print(f"you have {count} stations ")
+    i = 1
+    while i <= count:
+        station = letter_please(f"Enter station {i} name : ")
+        if station not in stations:
+            stations.append(station)
+            i += 1
+        else:
+            print(f"{RED}this station is already exist !{RESET}")
 
 

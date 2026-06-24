458e6758f752cd0162936d361e2f759f3f79901a
Manii <manieskandari05@gmail.com>
2026-06-24 12:39:13 +0330

    1.1 commit

---
+++ main.py
@@ -0,0 +1,29 @@
+admin_data = {
+    "username": "@admin1",
+    "password": "&admin1"
+}
+employees_data = {}
+travelers_data = {}
+
+# start panel
+
+while True:
+    user_input = int(input("""
+    Welcome!
+    1. Admin
+    2. Employee
+    3. Traveler
+    4. Exit!
+    -------------------------
+    Your choice? 
+    """))
+    match user_input:
+
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+            break

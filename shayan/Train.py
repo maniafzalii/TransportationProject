9f2327849ad7102f11dccf0d89d1101418ab7e99
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-06-25 19:14:59 +0330

    init is here for Shiva - Train is here for Mani

--- shayan/Train.py
+++ shayan/Train.py
@@ -1,5 +1,5 @@
 class Train():
-    def __init__(self, name, move_line, speed: int, waiting_time: int, stars, ticket_price: int, capacity: int):
+    def __init__(self, name, move_line, speed: int, waiting_time: int, stars, ticket_price: int, capacity: int , id : int):
         self.name = name
         self.moveline = move_line
         self.speed = speed
@@ -7,15 +7,15 @@ class Train():
         self.stars = stars
         self.ticket_price = ticket_price
         self.capacity = capacity
-        self.id = 1  # default for now - you can change it
+        self.id = id
 
     def __str__(self):
-        return f"{BLUE}{self.id}---->{RESET}     name: {self.name} -- move_line: {self.moveline} -- average speed:{self.speed}" \
-               f" -- waiting_time: {self.waiting_time} -- stars : {self.stars} -- ticket_price: {self.ticket_price} -- capacity : {self.capacity}"
+        return f"ID: {self.id}   name: {self.name}    move_line: {self.moveline}    average speed: {self.speed}" \
+               f"    waiting_time: {self.waiting_time}    stars:  {self.stars}    ticket_price: {self.ticket_price} " \
+               f"   capacity: {self.capacity}"
 
 
 trains = {}
-
 BLUE = '\033[94m'
 RESET = '\033[0m'
 RED = '\033[91m'
